d398b96395e4b380cef7c06c70f528ddad1fbcbc
XMINH-2028 <minhnguyenx28@gmail.com>
2020-12-04 22:26:00 +0700

    kdsfsf

--- index.js
+++ index.js
@@ -157,7 +157,6 @@ $('.signup').addEventListener('click',()=>{
 
 for (let i=0;i<$$('input').length;i++) {
 	$$('input')[i].addEventListener('focus',()=>{
-		$('.zero').classList.remove('zerohd');
 		setInterval(()=>{
 			if (dcheight<180 ) {
 				$('.bagua').classList.add('bgscale5');
